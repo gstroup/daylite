96147cac0e55a5c79232c08c21a4ee80cd499cbe
Greg Stroup <gstroup@gmail>
2012-06-08 13:56:05 -0700

    Greg: got rid of some global-type variables.  now more modular.

--- demo.js
+++ demo.js
@@ -5,11 +5,12 @@ define(["js/daylite"], function (daylite) {
   //document.addEventListener( "DOMContentLoaded", function(){
 
     //window.alert("calling daylite init");  
-  //var calendar, calendar2
+  var calendar, calendar2;
   
-  daylite.init(
+  calendar = daylite("picker");
+  calendar.init(
     {
-      container: "picker",
+      //container: "picker",
       dateFormat: "MM/DD/YYYY",
       specialDate: new Date(2012, 4, 25), 
       disableWeekends: true,
@@ -17,33 +18,36 @@ define(["js/daylite"], function (daylite) {
       minDate: new Date(2012, 4, 8),
       maxDate: new Date(2012, 7, 15),
       swipeEnabled: false,
-      language: "en",
-      useTwitterBootstrap: true
+      //language: "en",
+      useTwitterBootstrap: true,
+      formFieldId: "dateField"
     }
   );
   
-  // daylite2.init(
-  //   {
-  //     container: "secondPicker",
-  //     dateFormat: "MM/DD/YYYY",
-  //     specialDate: new Date(2012, 4, 25), 
-  //     disableWeekends: true,
-  //     disableDates: [new Date(2012, 4, 28), new Date(2012, 6, 4)], 
-  //     minDate: new Date(2012, 4, 8),
-  //     maxDate: new Date(2012, 7, 15),
-  //     swipeEnabled: false,
-  //     language: "fr",
-  //     useTwitterBootstrap: true
-  //     // dateSelectedHandler: function(jsDate) {
-  //     //   window.alert("Date was selected: " + jsDate);
-  //     //   document.getElementById("picker").style.display = "none";
-  //     // }
-  //   }
-  // );
+  calendar2 = daylite("secondPicker");
+  calendar2.init(
+    {
+      //container: "secondPicker",
+      dateFormat: "MM/DD/YYYY",
+      specialDate: new Date(2012, 4, 25), 
+      disableWeekends: false,
+      disableDates: [new Date(2012, 4, 28), new Date(2012, 6, 4)], 
+      minDate: new Date(2012, 3, 8),
+      maxDate: new Date(2012, 8, 15),
+      swipeEnabled: false,
+      //language: "fr",
+      useTwitterBootstrap: true,
+      formFieldId: "secondDateField",
+      dateSelectedHandler: function(jsDate) {
+        window.alert("Date was selected: " + jsDate);
+        document.getElementById("secondPicker").style.display = "none";
+      }
+    }
+  );
   
-  document.getElementById('dateField').addEventListener("click", function () { daylite.open('dateField'); }, false);
+  document.getElementById('dateField').addEventListener("click", function () { calendar.open('dateField'); }, false);
   
-  // document.getElementById('secondDateField').addEventListener("click", function () { daylite2.open('secondDateField'); }, false);  
+  document.getElementById('secondDateField').addEventListener("click", function () { calendar2.open('secondDateField'); }, false);  
   
   //});
 
--- js/daylite.js
+++ js/daylite.js
@@ -1,37 +1,36 @@
 define(["moment"], function(moment) {
 
-//var daylite = function() {
-  var settings, container, selectedDate, 
-      dateInCurrentMonth, formFieldId;
-  
   function init(options) {
-    //window.alert("init called. " + options.container);
-    var o;
-    settings = {
-      dateFormat: "MM/DD/YYYY",
-      disableWeekends: false,
-      disableDates: null, 
-      specialDate: null, 
-      minDate: null, 
-      maxDate: null,
-			swipeEnabled: false,
-      language: "en",
-      useTwitterBootstrap: true
-    };
+    var o, 
+      settings = {
+        dateFormat: "MM/DD/YYYY",
+        disableWeekends: false,
+        disableDates: null, 
+        specialDate: null, 
+        minDate: null, 
+        maxDate: null,
+        swipeEnabled: false,
+        language: "en",
+        useTwitterBootstrap: true
+      };
     for (o in options) {
       if (options.hasOwnProperty(o)) {
         settings[o] = options[o];
       }
     }
+    //console.log("init lang: " + settings.language);      
     settings.specialDate = settings.specialDate ? moment(settings.specialDate) : null;
     settings.minDate = settings.minDate ? moment(settings.minDate) : null;
     settings.maxDate = settings.maxDate ? moment(settings.maxDate) : null;
-    moment.lang(settings.language);
-    container = document.getElementById(settings.container);
+    //moment.lang(settings.language);
+    moment.lang("en");
+    //console.log("moment lang: " + moment.lang());
+    
+    return settings;
   }
-  
+
   //// Handlers
-  function incrementMonth(monthsToAdd) {
+  function incrementMonth(monthsToAdd, dateInCurrentMonth, container, settings) {
     if (settings.minDate && monthsToAdd < 0) {
       if (settings.minDate.diff( dateInCurrentMonth.clone().date(1) ) > 0) {
         return false;
@@ -42,36 +41,36 @@ define(["moment"], function(moment) {
         return false;
       }
     }
-    
+  
     dateInCurrentMonth.add("M", monthsToAdd);
     //console.log("dateInCurrentMonth: " + dateInCurrentMonth);
-    updateGrid(dateInCurrentMonth);
+    updateGrid(dateInCurrentMonth, container, settings);
   }
-  
-  function onDateSelected(e) {
+
+  function onDateSelected(e, container, dateInCurrentMonth, settings) {
     var momentSelected, dayOfMonth;
     if (e.target.className.indexOf("dl-day-disabled")>0) {
       return false;
     }
-    // TODO: trim?? use an attribute instead of innerHTML?  this.replace(/^\s+|\s+$/g, "");
+    // TODO: trim?? use a data- attribute instead of innerHTML?  this.replace(/^\s+|\s+$/g, "");
     dayOfMonth = e.target.innerHTML;
     momentSelected = dateInCurrentMonth.date(dayOfMonth);
     if (settings.dateSelectedHandler) {
       settings.dateSelectedHandler(momentSelected.toDate());
     } else {
-      document.getElementById(formFieldId).value = momentSelected.format(settings.dateFormat);
+      document.getElementById(settings.formFieldId).value = momentSelected.format(settings.dateFormat);
       container.style.display = "none"; 
     }
   }
-  
-  function addHandlers() {
-    container.getElementsByClassName('dl-prev')[0].addEventListener("click", function () { incrementMonth(-1); }, false);
-    container.getElementsByClassName('dl-next')[0].addEventListener("click", function () { incrementMonth(1); }, false);
-    container.getElementsByClassName('dl-grid')[0].addEventListener("click", function (e) { onDateSelected(e);}, false);
+
+  function addHandlers(container, dateInCurrentMonth, settings) {
+    container.getElementsByClassName('dl-prev')[0].addEventListener("click", function () { incrementMonth(-1, dateInCurrentMonth, container, settings); }, false);
+    container.getElementsByClassName('dl-next')[0].addEventListener("click", function () { incrementMonth(1, dateInCurrentMonth, container, settings); }, false);
+    container.getElementsByClassName('dl-grid')[0].addEventListener("click", function (e) { onDateSelected(e, container, dateInCurrentMonth, settings);}, false);
   }
-  
+
   //// Html
-  function buildHtmlForDay(date, htmlDays) {
+  function buildHtmlForDay(date, htmlDays, dateInCurrentMonth, settings) {
     var dayOfMonth = date.date();
     if (settings.disableWeekends && (date.day() === 0 || date.day() === 6)) {
       return "<div class='dl-day dl-day-disabled'>" + dayOfMonth + "</div>";      
@@ -81,24 +80,22 @@ define(["moment"], function(moment) {
       return "<div class='dl-day dl-day-disabled'>" + dayOfMonth + "</div>";      
     } else if (settings.maxDate && date.diff(settings.maxDate, 'days') > 0) {
       return "<div class='dl-day dl-day-disabled'>" + dayOfMonth + "</div>";            
-    } else if (settings.specialDate && date.diff(settings.specialDate) === 0) {
+    } else if (settings.specialDate && date.diff(settings.specialDate, 'days') === 0) {
       return "<div class='dl-day dl-day-special'>" + dayOfMonth + "</div>";
     }
     return "<div class='dl-day'>" + dayOfMonth + "</div>";
-    //return "<div class='day'><a href='" + dayOfMonth + "'>" + dayOfMonth + "</a></div>";
-    //return "<div class='day' onclick='daylite.onDateSelected(" + dayOfMonth + ");'>" + dayOfMonth + "</div>";
   }
-  
-  function buildHtmlForWeek(datesInWeek, htmlDays) {
+
+  function buildHtmlForWeek(datesInWeek, htmlDays, dateInCurrentMonth, settings) {
     var i;
     htmlDays.push("<div class='dl-week'>");
     for (i=0; i<datesInWeek.length; i++) {
-      htmlDays.push(buildHtmlForDay(datesInWeek[i], htmlDays));
+      htmlDays.push(buildHtmlForDay(datesInWeek[i], htmlDays, dateInCurrentMonth, settings));
     }
     htmlDays.push("</div>");
   }
-  
-  function displayHeader(anyDate, htmlDays) {
+
+  function displayHeader(anyDate, htmlDays, settings) {
     // TODO: use images for prev, next, add close
     if (settings.useTwitterBootstrap) {
       htmlDays.push("<div class='dl-head'><div class='dl-prev'><i class='icon-chevron-left'></i></div>");      
@@ -112,26 +109,25 @@ define(["moment"], function(moment) {
       htmlDays.push("<div class='dl-next'>>></div></div>");      
     }
   }
-  
-  function updateGrid(fieldValue) {
+
+  function updateGrid(fieldValue, container, settings) {
     var daysInAWeek = [],
         htmlDays = [],
-        daysInMonth,
         prevMonthDays, nextMonthDays,
-        i,j,k, dateI;
-    selectedDate = fieldValue ? moment(fieldValue) : moment(new Date());
-    dateInCurrentMonth = dateInCurrentMonth || selectedDate;
-    daysInMonth = selectedDate.daysInMonth();
-    
+        i,j,k, dateI,
+        selectedDate = fieldValue ? moment(fieldValue) : moment(new Date()),
+        dateInCurrentMonth = dateInCurrentMonth || selectedDate,
+        daysInMonth = selectedDate.daysInMonth();
+  
     htmlDays.push("<div class='dl-container'>");
-    displayHeader(selectedDate, htmlDays);
+    displayHeader(selectedDate, htmlDays, settings);
     htmlDays.push("<div class='dl-grid'>");
-    
+  
     for (i=1; i<=daysInMonth; i++) {
       // TODO: clean up !!!!!
       //console.log("i=" + i);
       dateI = selectedDate.clone().date(i);
-      
+    
       // days from prev month
       if (dateI.date()===1 && dateI.day() > 0) {
         prevMonthDays = dateI.day();
@@ -140,9 +136,9 @@ define(["moment"], function(moment) {
           daysInAWeek.push(dateI.clone().subtract('days', j));
         }
       } 
-      
+    
       daysInAWeek.push(dateI);
-      
+    
       // days from next month
       if (i===daysInMonth && dateI.day() < 6) {
         nextMonthDays = 6 - dateI.day();
@@ -150,39 +146,42 @@ define(["moment"], function(moment) {
           daysInAWeek.push(dateI.clone().add('days', k));
         }
       }
-      
+    
       if (dateI.day() === 6) {
-        buildHtmlForWeek(daysInAWeek, htmlDays);
+        buildHtmlForWeek(daysInAWeek, htmlDays, dateInCurrentMonth, settings);
         // start a new week row
         daysInAWeek = [];
       }
-      
-      if (i===daysInMonth) {
-        buildHtmlForWeek(daysInAWeek, htmlDays);
-      }
-         
-    }
     
+      if (i===daysInMonth) {
+        buildHtmlForWeek(daysInAWeek, htmlDays, dateInCurrentMonth, settings);
+      }
+       
+    }
+  
     htmlDays.push("</div></div>");
 
     container.innerHTML = htmlDays.join(" ");
       //_.template("<div><%=(x)%></div>", "data");
-      
-    addHandlers();
+    
+    addHandlers(container, dateInCurrentMonth, settings);
   }
-  
-  function open(fieldId) {
-    //window.alert("open called." + document.getElementById(formFieldId).value );
-    formFieldId = fieldId;
-    updateGrid(document.getElementById(fieldId).value);
-    container.style.display = "block";
-  }
-  
-  return {
-    init: init,
-    open: open,
-    onDateSelected: onDateSelected
-  };
-//}
 
+  function constructor(containerSelector) {
+    var newDaylite = {},
+        container = document.getElementById(containerSelector);
+    
+    newDaylite.settings = {};
+    newDaylite.init = function(options) {
+      newDaylite.settings = init(options);
+    };
+    newDaylite.open = function(fieldId) {
+      updateGrid(document.getElementById(fieldId).value, container, newDaylite.settings);
+      container.style.display = "block";      
+    };
+    
+    return newDaylite;
+  }
+
+  return constructor;
 });
